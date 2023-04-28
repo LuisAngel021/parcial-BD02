1ee69dda69d0e0e911750ec4fc1215dd53d742fb
LuisAngel021 <110065864+LuisAngel021@users.noreply.github.com>
2023-04-28 11:49:42 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
+import boto3
 import requests
+import datetime
 
-def test_url_response():
-    response = requests.get('https://ejemplo.com')
-    assert response.status_code == 200
-    assert response.headers['Content-Type'] == 'text/html; charset=UTF-8'
+client = boto3.client('s3')
+BUCKET_NAME = 'headlines021'
+
+
+def capturar_html():
+    url = 'https://www.eltiempo.com'
+    url2 = 'https://www.elespectador.com'
+    response = requests.get(url)
+    response2 = requests.get(url2)
+    html_file = f"{datetime.datetime.now().strftime('%Y-%m-%d')}.html"
+    html_file2 = f"Es{datetime.datetime.now().strftime('%Y-%m-%d')}.html"
+    client.put_object(Bucket=BUCKET_NAME, Key='raw/'+html_file, Body=response.content)
+    client.put_object(Bucket=BUCKET_NAME, Key='raw/'+html_file2, Body=response2.content)
